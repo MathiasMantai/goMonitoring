93be95ce7f5a01c82f55cd42b9046a617e1f03ca
mathiasmantai <mmantaibusiness@gmail.com>
2023-06-20 07:49:42 +0000

    feat: serving css

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"fmt"
+)
+
+const (
+	port = "8080"
 )
 
 type PageData struct {
@@ -10,21 +15,24 @@ type PageData struct {
 	Body string
 }
 
-
-
 func main() {	
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := PageData {
 			Title: "Monitoring",
 			Body: "Test",
 		}
 
-		renderTemplate(w, "templates/index.html", data)
+		renderTemplate(w, "web/index.html", data)
 	})
 
-	http.ListenAndServe(":8080", nil)
-}
+	//serve css
+	styles := http.FileServer(http.Dir("./web/css"))
+	http.Handle("/css/", http.StripPrefix("/css/", styles))
 
+	fmt.Println("starting webserver on port " + port)
+	http.ListenAndServe(":" + port, nil)
+}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	t, err := template.ParseFiles(tmpl)
