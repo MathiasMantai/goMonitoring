f07759bd78891b128c5b0a2aac0e98866d06d90b
mathiasmantai <mmantaibusiness@gmail.com>
2023-06-23 10:52:53 +0000

    feat: cpu, container and network page routing + html templates

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+/*
+	project: goMonitoring
+	version: 1.0
+	author: Mathias Mantai <mmantaibusiness@gmail.com>
+*/
+
 import (
 	"html/template"
 	"net/http"
@@ -13,18 +19,10 @@ const (
 type PageData struct {
 	Title string
 	Body string
+	CurrentRoute string
 }
 
-func main() {	
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := PageData {
-			Title: "Monitoring",
-			Body: "Test",
-		}
-		renderTemplate(w, "web/index.html", data)
-	})
-
+func serveStaticFiles() {
 	//serve css
 	styles := http.FileServer(http.Dir("./web/css"))
 	http.Handle("/css/", http.StripPrefix("/css/", styles))
@@ -37,9 +35,50 @@ func main() {
 	img := http.FileServer(http.Dir("./web/img"))
 	http.Handle("/img/", http.StripPrefix("/img/", img))
 
-	//serve external libraries
-	img := http.FileServer(http.Dir("./web/img"))
-	http.Handle("/img/", http.StripPrefix("/img/", img))
+	pages := http.FileServer(http.Dir("./web/pages"))
+	http.Handle("/pages/", http.StripPrefix("/pages/", pages))
+}
+
+func main() {	
+
+	pages := []string{
+		"web/index.html", 
+		"web/pages/cpu.html", 
+		"web/pages/container.html",
+		"web/pages/network.html",
+	}
+
+	//default route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData {
+			Title: "Monitoring",
+			Body: "Test",
+			CurrentRoute: "/",
+		}
+		renderTemplateWithContent(w, data, pages...)
+	})
+
+	//container route
+	http.HandleFunc("/container", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData {
+			Title: "Monitoring",
+			Body: "Test",
+			CurrentRoute: "/container",
+		}
+		renderTemplateWithContent(w, data, pages...)
+	})
+
+	http.HandleFunc("/network", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData {
+			Title: "Monitoring",
+			Body: "Test",
+			CurrentRoute: "/network",
+		}
+		renderTemplateWithContent(w, data, pages...)
+	})
+
+	//serve static files
+	serveStaticFiles()
 
 	fmt.Println("starting webserver on port " + port)
 	http.ListenAndServe(":" + port, nil)
@@ -47,6 +86,21 @@ func main() {
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	t, err := template.ParseFiles(tmpl)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, data)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func renderTemplateWithContent(w http.ResponseWriter,data interface{}, content ...string) {
+	t, err := template.ParseFiles(content...)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
--- web/js/main.js
+++ web/js/main.js
@@ -1,130 +1,8 @@
-function createDonutChart()
-{
-    console.log("test")
-}
-
-createDonutChart()
-
-
-{/* <script>
-    export default {
-        data() {
-            return {
-                totalData: 0,
-                svgRingParts: [],
-                viewbox: ""
-            }
-        },
-        props: {
-            radius: {
-                type: Number,
-                default: 100
-            },
-            ringWidth: {
-                type: Number,
-                default: 10
-            },
-            data: {
-                type: Array,
-                default: [10,10,10,10,10]
-            },
-            color: {
-                type: Array,
-                default: ["#ff6200", "green", "red", "blue", "#fff"]
-            }
-
-        },
-        methods: {
-            arcEndPoint(angle, radius, totalSize) {
-                return  [
-                    Math.cos(angle * Math.PI / 180) * radius + totalSize / 2,
-                    Math.sin(angle * Math.PI / 180) * -radius + totalSize / 2
-                ]
-            },
-            calcTotalData(data) {
-                let tmp = 0;
-                data.map(dataElement => tmp += dataElement);
-                return tmp;
-            },
-            dataToAngle(dataElement, totalData) {
-                let dataElementPercentage = (dataElement * 100) / totalData;
-                let angle = (dataElementPercentage * 360) / 100;
-                // console.log(dataElement, totalData);
-                return angle;
-            },
-            outputChart() {
-                return h('div', {id: 'test'}, []);
-            }
-        },
-        mounted() {
-            //calculate sum of data
-            this.totalData = this.calcTotalData(this.data);
-            
-            //calculate angles based on percentage of 360 degrees
-            let startX = this.radius*2;
-            let startY = this.radius;
-            let angleSum = 0;
-            let cnt = 0;
-            this.data.map( (dataElement) => {
-                let currentAngle = this.dataToAngle(dataElement, this.totalData);
-                let largeArcFlag = (currentAngle > 180 ? 1 : 0);
-
-                //offset for 
-                let offset = 360 - angleSum;
-
-                //sum of angles for next offset
-                angleSum += currentAngle;
-
-                //endpoint for outer arc
-                let currentEndPoint = this.arcEndPoint(currentAngle, this.radius, this.radius * 2);
-
-                //endpoint for inner arc
-                let currentEndPointInner =  this.arcEndPoint(currentAngle, this.radius - this.ringWidth, this.radius * 2);
-                console.log(offset);
-                //path commands
-                this.svgRingParts.push({path: "M " + startX + " " + startY + " " + "A " + this.radius + " " + this.radius + " 0 " + largeArcFlag + " 0 " + currentEndPoint[0] + " " + currentEndPoint[1] + " L " + currentEndPointInner[0] + " " + currentEndPointInner[1] + " A " + (this.radius - this.ringWidth) + " " + (this.radius - this.ringWidth) + " 0 " + largeArcFlag + " 1" + (startX - this.ringWidth) + " " + startY, color: this.color[cnt], offset: offset});
-                cnt++;
-            });
-
-            //viewbox
-            this.viewbox = "0 0 " + this.radius*2 + " " + this.radius*2;
-        }
-    }
-</script>
-
-<template>
-    <svg :viewbox="viewbox" :width="(radius * 2)" :height="(radius * 2)">
-        <path v-for="path in svgRingParts" :d="path.path" :fill="path.color" style="transform-origin: center;" :transform="'rotate('+path.offset+')'" />
-    </svg>
-</template> */}
-
-
-
-let c = document.getElementById("chart")
-// new Chart(c, {
-//     type: 'bar',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   });
-
-  var data = {
+var data = {
     labels: ['Rot', 'Grün'],
     datasets: [{
         data: [30, 50], // Angenommene Datenwerte
-        backgroundColor: ['#ff6384', '#ddd', 'transparent']
+        backgroundColor: ['#38B3F8', '#ddd']
     }]
 };
 
@@ -134,9 +12,12 @@ var options = {
     responsive: false,
     maintainAspectRatio: false,
     plugins: {
+        customCanvasBackgroundColor: {
+            color: '#38B3F8'
+        },
         legend: {
             display: false
-        }
+        },
     }
 };
 
