1796930ab12f6b86a872abb6c16db1be86a5e190
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-04 07:26:16 +0000

    chore: minify css

--- web/js/main.js
+++ web/js/main.js
@@ -7,8 +7,9 @@ if(window.location.href.includes("/network"))
     const chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', 
-                 '10', '11','12','13','14','15','16','17','18','19','20'
+        labels: [
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+                    '10', '11','12','13','14','15','16','17','18','19','20'
                 ],
         datasets: [
         {
@@ -143,7 +144,6 @@ else
         .then(response => response.json())
         .then(
             (data) => {
-                console.log(data)
                 cpu = data.CpuUsage.toFixed(2)
                 vMemory = data.VirtualMemory.toFixed(2)
                 updateChart(donutChart, [parseFloat(100-cpu), cpu])
